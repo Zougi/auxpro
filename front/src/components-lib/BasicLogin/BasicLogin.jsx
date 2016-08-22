f9100f24819ce5104599d239cc4c7a1e28dbb6cc
ash.ruins66 <ash.uncover@gmail.com>
2016-08-22 21:49:10 +0200

    forgotten file

--- front/src/components-lib/BasicLogin/BasicLogin.jsx
+++ front/src/components-lib/BasicLogin/BasicLogin.jsx
@@ -12,54 +12,52 @@ import IForm from '../Form/IForm.jsx';
  *             passTitle='PASSTITLE'/>
  */ 
 
-let FIELDS = [	
-	{	
-		name: 'user',
-		type: 'email',
-		id: 'user',
-		label: this.props.userTitle || 'Adresse électronique',
-		placeholder: this.props.userTitle || 'Adresse électronique'
-	},
-	{	
-		name: 'pass',
-		type: 'password',
-		id: 'pass',
-		label: this.props.passTitle || 'Mot de passe',
-		placeholder: this.props.passTitle || 'Mot de passe'
-	}
-];
-
-let BUTTONS = {
-	submit: {
-		text: 'Connexion',
-		bsStyle:'success',
-		bsSize:'large'
-	},
-	cancel: {
-			text: 'Annuler',
-			bsStyle: 'default',
-			bsSize: 'large',
-			linkContainer: '/'
-	}
-};
-
 class BasicLogin extends React.Component {
 
 	constructor(props) {
 		super(props);
+		this.FIELDS = [	
+			{	
+				name: 'user',
+				type: 'email',
+				id: 'user',
+				label: this.props.userTitle || 'Adresse électronique',
+				placeholder: this.props.userTitle || 'Adresse électronique'
+			},
+			{	
+				name: 'pass',
+				type: 'password',
+				id: 'pass',
+				label: this.props.passTitle || 'Mot de passe',
+				placeholder: this.props.passTitle || 'Mot de passe'
+			}
+		];
+		this.BUTTONS = {
+			submit: {
+				text: 'Connexion',
+				bsStyle:'success',
+				bsSize:'large'
+			},
+			cancel: {
+					text: 'Annuler',
+					bsStyle: 'default',
+					bsSize: 'large',
+					linkContainer: '/'
+			}
+		};
 	}
   
 	render() {
 		return (
-				<Row>
-					<Col smOffset={1} sm={10} mdOffset={2} md={8}>
-						<Panel 
-							header={ this.props.error ? this.props.error : this.props.title || 'Saisir les informations utilisateur' } 
-							bsStyle={ this.props.error ? 'danger' : 'default' }>
-							<IForm onSubmit={this.props.onSubmit} fields={FIELDS} buttons={BUTTONS}/>
-						</Panel>
-					</Col>
-				</Row>
+			<Row>
+				<Col smOffset={1} sm={10} mdOffset={2} md={8}>
+					<Panel 
+						header={ this.props.error ? this.props.error : this.props.title || 'Saisir les informations utilisateur' } 
+						bsStyle={ this.props.error ? 'danger' : 'default' }>
+						<IForm onSubmit={this.props.onSubmit} fields={this.FIELDS} buttons={this.BUTTONS}/>
+					</Panel>
+				</Col>
+			</Row>
 		);
   }
 }
