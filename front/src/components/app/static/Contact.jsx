b9cb224769e9953b124a27e9b5d2d91476835227
ash.ruins66 <ash.uncover@gmail.com>
2016-09-22 20:55:15 +0200

    forgotten file

--- front/src/components/app/static/Contact.jsx
+++ front/src/components/app/static/Contact.jsx
@@ -1,21 +1,16 @@
 import React from 'react'
 
-import { Panel } from 'react-bootstrap';
-
-import BusyIndicator from 'components-lib/BusyIndicator/BusyIndicator.jsx'
-import GoogleChartPie from 'components-lib/charts/GoogleChartPie.jsx'
-
 class Contact extends React.Component {
 
 	constructor(props) {
 		super(props);
 	}
 
-	render() { return (
-		<Panel className='container'>
-			<GoogleChartPie/>
-		</Panel>
-	);}
+	render() { 
+		return (
+			<div/>
+		);
+	}
 }
 
 export default Contact;
